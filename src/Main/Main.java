80e6554b5721fca6ba51f295e91610a0f8ddb36d
nerrowcool <tim3882513@gmail.com>
2017-10-07 20:11:21 +0800

    update

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,17 +1,38 @@
-package Multiplication;
+package Main;
 
 import java.util.Scanner;
 
 public class Main {
-  
+	int size;
+	int[][][] matrix;
+	
 	public static void main(String[] args) {
+		
+		Scanner selection = new Scanner(System.in);
+		System.out.println("Select the function you want.");
+		System.out.println("---------------------------------");
+		System.out.println("A - Addition");
+		System.out.println("S - Subtraction");
+		System.out.println("M - Multiplication");
+		System.out.println("D - Determinant");
+		System.out.println("I - Inverse");
+		
+		
+		switch (selection.next().toUpperCase()) {
+			case "M" :
+				multiplication(size, matrix);
+				break;
+		}		
+	}
+	
+	public void input() {
 		Scanner scanS = new Scanner(System.in);
     
 
 		System.out.println("Please enter the size of the matrix :");
-		int size = scanS.nextInt();
+		size = scanS.nextInt();
 
-		int[][][] matrix = new int[2][size][size];
+		matrix = new int[2][size][size];
 		Scanner scanM = new Scanner(System.in);
 
 
@@ -23,22 +44,23 @@ public class Main {
 				}
 			}
 		}
-
+		
 		scanS.close();
 		scanM.close();
-
-		int a = 0;
+	}
+	
+	public void multiplication(int size, int [][][] matrix) {
+		int newy, newx, a = 0;
 		int[][] newMatrix = new int[size][size];
-		for (int newy = 0; newy < size; newy++) {
-			for (int newx = 0; newx < size; newx++) {
+		for (newy = 0; newy < size; newy++) {
+			for (newx = 0; newx < size; newx++) {
 				for (int i = 0; i < size; i++) {
 					newMatrix[newy][newx] += matrix[a][newy][i] * matrix[(a + 1)][i][newx];
 				}
 			}
-}
+		}
 
 
-		int newy, newx;
 		for (newy = 0; newy < size; newy++) {
 			System.out.print("{ ");
 			for (newx = 0; newx < size - 1; newx++) {
